da7a3f246566b85b6247b49a8f1b97e99cc280ec
Odual259 <123269406+Odual259@users.noreply.github.com>
2024-06-03 15:20:50 -0600

    Primer Commit
    Primer Commit

---
+++ Prograweb1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prograweb1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Página de descripción.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Página de contacto.";
+
+            return View();
+        }
+    }
+}
